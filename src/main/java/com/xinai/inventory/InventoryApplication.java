62adb39ee51dd1de50a796a5a13c6a536e8fcead
bissolkynaston-ctrl <bissolkynaston@gmail.com>
2026-05-30 15:21:57 +0800

    Fix: add @MapperScan for MyBatis

--- src/main/java/com/xinai/inventory/InventoryApplication.java
+++ src/main/java/com/xinai/inventory/InventoryApplication.java
@@ -1,11 +1,13 @@
 package com.xinai.inventory;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@MapperScan("com.xinai.inventory.dao")
 public class InventoryApplication extends SpringBootServletInitializer {
 
     @Override
